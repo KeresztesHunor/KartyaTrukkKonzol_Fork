bf2ab64ee5cdeebee1859719e8de0e7228ddc4dc
kereszteshunor <kereszteshunor36@gmail.com>
2023-09-25 08:25:27 +0200

    OOP kesz

--- src/kartyatrukkkonzolon/Pakli.java
+++ src/kartyatrukkkonzolon/Pakli.java
@@ -53,16 +53,16 @@ public class Pakli
             case 2:
                 for (int i = 1; i <= 7; i++)
                 {
-                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i] = pakli[21 - (i - 1) * 3];
                     ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
-                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[19 - (i - 1) * 3];
                 }
                 break;
             case 3:
                 for (int i = 1; i <= 7; i++)
                 {
-                    ujPakli[i] = pakli[21 - (i - 1) * 3];
-                    ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[21 - (i - 1) * 3];
                     ujPakli[i + 14] = pakli[20 - (i - 1) * 3];
                 }
                 break;
